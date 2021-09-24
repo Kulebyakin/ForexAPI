7b2693812d0036239077f371fd2265dfecf64909
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-24 23:43:09 +0800

    Create account spec

---
+++ spec/requests/account_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+RSpec.describe "Account", :type => :request do
+before(:each) do
+    @user = FactoryBot.create(:user) 
+    @currency = FactoryBot.create(:currency)
+    @create_accaount_url = '/api/v1/accounts'
+    @account_params = {
+      amount: 23.9, 
+      user_id: 1, 
+      currency_id: 1
+    }
+    @invalid_account_params = {
+      amount: '', 
+      user_id: -1, 
+      currency_id: 0
+    }
+  end
+describe 'POST /api/v1/accounts' do
+context 'when account params is valid' do
+      before do
+        post @create_accaount_url, params: @account_params, as: :json
+      end
+it 'returns status 201' do
+      expect(response).to have_http_status(201)
+    end
+  end
+context 'when account params is invalid' do
+    before { post @create_accaount_url, params: @invalid_account_params, as: :json }
+it 'returns unathorized status 401' do
+      expect(response.status).to eq 401
+    end
+  end
+end
+end
