1765bc2c4285a491ad9ac2aab7f0b9e6b51c4c80
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-26 23:56:06 +0800

    Add route to currency#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :accounts
+      resources :currency, only: :index
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
