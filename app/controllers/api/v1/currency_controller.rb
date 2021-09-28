dc44299a3651936ae4b34dab72953d04f4b7ee2a
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-28 23:00:57 +0800

    Authenticate_user

--- app/controllers/api/v1/currency_controller.rb
+++ app/controllers/api/v1/currency_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
     class CurrencyController < ApplicationController
-      before_action :authenticate_api_v1_user!
-      
+      before_action :authenticate_user!
+
       def index
         @currencies = Currency.all
     
