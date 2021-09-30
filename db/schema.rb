2eba095e4a9f79c2a7667d1cd129d044dae7f017
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-30 15:53:06 +0800

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_123235) do
+ActiveRecord::Schema.define(version: 2021_09_30_044827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2021_09_21_123235) do
     t.string "ticker", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "from_account_id", null: false
+    t.integer "to_account_id", null: false
+    t.integer "amount", null: false
+    t.float "desired_exchange_rate", null: false
+    t.datetime "expiry_date", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,4 +77,5 @@ ActiveRecord::Schema.define(version: 2021_09_21_123235) do
 
   add_foreign_key "accounts", "currencies"
   add_foreign_key "accounts", "users"
+  add_foreign_key "orders", "users"
 end
