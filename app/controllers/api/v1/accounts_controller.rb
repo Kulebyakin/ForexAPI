88700beb5a84f01483f889cbc09dac494a06b09c
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-30 22:37:29 +0800

    Change the way to write modules

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,62 +1,57 @@
-module Api
-  module V1
-    class AccountsController < ApplicationController
-      before_action :set_account, only: [:show, :update, :destroy]
-      before_action :authenticate_user!
+class Api::V1::AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :update, :destroy]
+  before_action :authenticate_user!
 
-      after_action :verify_authorized, except: :index
-      after_action :verify_policy_scoped, only: :index
-    
-      # GET /accounts
-      def index
-        @accounts = policy_scope(Account.where(user: current_user))
-    
-        render json: @accounts
-      end
-    
-      # GET /accounts/1
-      def show
-        render json: @account
-      end
-    
-      # POST /accounts
-      def create
-        @account = Account.new(account_params)
-        authorize @account
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
 
-        if @account.save
-          render json: @account, status: :created
-        else
-          render json: @account.errors, status: :unprocessable_entity
-        end
-      end
-    
-      # PATCH/PUT /accounts/1
-      def update
-        if @account.update(account_params)
-          render json: @account
-        else
-          render json: @account.errors, status: :unprocessable_entity
-        end
-      end
-    
-      # DELETE /accounts/1
-      def destroy
-        @account.destroy
-      end
-    
-      private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_account
-          @account = Account.find(params[:id])
-          authorize @account
-        end
-    
-        # Only allow a list of trusted parameters through.
-        def account_params
-          params.require(:account).permit(:amount, :user_id, :currency_id)
-        end
-    end
-    
+  # GET /accounts
+  def index
+    @accounts = policy_scope(Account.where(user: current_user))
+
+    render json: @accounts
   end
+
+  # GET /accounts/1
+  def show
+    render json: @account
+  end
+
+  # POST /accounts
+  def create
+    @account = Account.new(account_params)
+    authorize @account
+
+    if @account.save
+      render json: @account, status: :created
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /accounts/1
+  def update
+    if @account.update(account_params)
+      render json: @account
+    else
+      render json: @account.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /accounts/1
+  def destroy
+    @account.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_account
+      @account = Account.find(params[:id])
+      authorize @account
+    end
+
+    # Only allow a list of trusted parameters through.
+    def account_params
+      params.require(:account).permit(:amount, :user_id, :currency_id)
+    end
 end
