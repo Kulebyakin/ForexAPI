babc61ed5ff93bd974f3ed16e23907f91a470fdf
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-30 16:19:08 +0800

    Setup AASM for order status

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,27 @@ class Order < ApplicationRecord
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 1 }
   validates :desired_exchange_rate, presence: true
   validates :expiry_date, presence: true
+
+  include AASM
+
+  aasm(:status) do
+    state :created, initial: true
+    state :pending
+    state :filled
+    state :canceled
+
+    event :pend do
+      transitions from: :created, to: :pending
+    end
+
+    event :fill do
+      transitions from: :pending, to: :filled
+    end
+
+    event :cancel do
+      transitions from: :pending, to: :canceled
+      transitions from: :created, to: :canceled
+    end
+
+  end
 end
