f09febb4599339e161d6f2326228b59662aeac15
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-09-30 15:53:19 +0800

    Add orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  
   mount_devise_token_auth_for 'User', at: 'auth'
+  
   namespace :api do
     namespace :v1 do
-      
       resources :accounts
       resources :currency, only: :index
+      resources :orders
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
