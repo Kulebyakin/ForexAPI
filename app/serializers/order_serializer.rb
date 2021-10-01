b8f9957ecd2a2a532bfb117302bc4f5c4070ee03
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-01 23:23:20 +0800

    Add status field to order serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :desired_exchange_rate, :expiry_date
+  attributes :id, :amount, :desired_exchange_rate, :expiry_date, :status
   has_one :user
   has_one :from_account
   has_one :to_account
