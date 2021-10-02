864069f18b51a327ae3382bfe437bb54feaf4809
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-02 18:20:08 +0800

    Fix errors; Pend order if there is not enough money

--- app/workers/order_worker.rb
+++ app/workers/order_worker.rb
@@ -11,12 +11,13 @@ class OrderWorker
     else
       response =  RestClient.get "https://www.alphavantage.co/query?"\
         "function=CURRENCY_EXCHANGE_RATE&"\
-        "from_currency=#{order.from_account.currency.ticker}&"\
-        "to_currency=#{order.to_account.currency.ticker}&"\
+        "from_currency=#{order.to_account.currency.ticker}&"\
+        "to_currency=#{order.from_account.currency.ticker}&"\
         "apikey=#{Rails.application.credentials.alphavantage_apikey}", 
         {content_type: :json, accept: :json}
 
-      current_exchange_rate = JSON.parse(response.body)["Realtime Currency Exchange Rate"]["5. Exchange Rate"].to_f
+      current_exchange_rate = JSON.parse(
+        response.body)["Realtime Currency Exchange Rate"]["5. Exchange Rate"].to_f
 
       if order.desired_exchange_rate < current_exchange_rate
         OrderWorker.perform_at(5.seconds.from_now, id)
@@ -24,14 +25,17 @@ class OrderWorker
         from_account = Account.find(order.from_account_id)
         to_account = Account.find(order.to_account_id)
 
-        new_from_amount = from_account.amount + order.amount * current_exchange_rate
-        from_account.update(amount: new_from_amount)
+        new_amount_from_account = from_account.amount - order.amount * current_exchange_rate
+        if new_amount_from_account < 0
+          order.pend
+          order.save
+        else
+          from_account.update(amount: new_amount_from_account)
+          to_account.update(amount: to_account.amount + order.amount)
 
-        new_to_amount = to_account.amount - order.amount * current_exchange_rate
-        to_account.update(amount: new_to_amount)
-
-        order.fill
-        order.save
+          order.fill
+          order.save
+        end
       end
     end
   end
