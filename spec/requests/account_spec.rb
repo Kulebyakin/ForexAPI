44eb2a5e2d97e3d7b4c44e2330a0cdc49f597c09
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-03 20:29:50 +0800

    Add request specs

--- spec/requests/account_spec.rb
+++ spec/requests/account_spec.rb
@@ -1,34 +1,57 @@
 require "rails_helper"
 RSpec.describe "Account", :type => :request do
-before(:each) do
-    @user = FactoryBot.create(:user) 
-    @currency = FactoryBot.create(:currency)
-    @create_accaount_url = '/api/v1/accounts'
+  before(:each) do
+    user = FactoryBot.create(:user) 
+    currency = FactoryBot.create(:currency)
+    @accounts_url = '/api/v1/accounts'
     @account_params = {
       amount: 23.9, 
-      user_id: 1, 
-      currency_id: 1
+      user_id: user.id, 
+      currency_id: currency.id
     }
-    @invalid_account_params = {
-      amount: '', 
-      user_id: '', 
-      currency_id: ''
+    @sign_in_url = '/auth/sign_in' 
+    @login_params = {
+      email: user.email,
+      password: user.password
     }
+    post @sign_in_url, params: @login_params, as: :json
+    @headers = response.headers
   end
-describe 'POST /api/v1/accounts' do
-context 'when account params is valid' do
+
+  describe 'POST /api/v1/accounts' do
+    context 'when account params is valid' do
       before do
-        post @create_accaount_url, params: @account_params, as: :json
+        post @accounts_url, headers: @headers, params: @account_params, as: :json
       end
-it 'returns status 201' do
-      expect(response).to have_http_status(201)
+      it 'returns status 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+    context 'when account params is invalid' do
+      before { post @accounts_url, params: @invalid_account_params, as: :json }
+      it 'returns unathorized status 401' do
+          expect(response).to have_http_status(401)
+      end
     end
   end
-context 'when account params is invalid' do
-    before { post @create_accaount_url, params: @invalid_account_params, as: :json }
-it 'returns unathorized status 401' do
-      expect(response.status).to eq 422
+
+  describe 'GET /api/v1/accounts' do
+    before do
+      get @accounts_url, headers: @headers, as: :json
+    end
+    it 'returns status 200' do
+      expect(response).to have_http_status(200)
     end
   end
-end
+
+  describe "GET /api/v1/accounts/1" do
+    before do
+      @account = Account.create! @account_params
+    end
+    it "renders a successful response" do
+      get api_v1_accounts_url(@account), headers: @headers, as: :json
+      expect(response).to be_successful
+    end
+  end
+
 end
--- spec/requests/currency_spec.rb
+++ spec/requests/currency_spec.rb
@@ -1,10 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "Currencies", type: :request do
+  before(:each) do
+    @user = FactoryBot.create(:user) 
+    @currency = FactoryBot.create(:currency)
+    @sign_in_url = '/auth/sign_in' 
+    @login_params = {
+        email: @user.email,
+        password: @user.password
+    }
+    post @sign_in_url, params: @login_params, as: :json
+    @headers = response.headers
+  end
   describe "GET /index" do
     it "returns http success" do
-      get "/currency/index"
+      get "/api/v1/currency", headers: @headers
       expect(response).to have_http_status(:success)
+    end
+  end
+  describe "GET /index without login" do
+    it "returns http success" do
+      get "/api/v1/currency"
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
