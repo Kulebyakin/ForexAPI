0a9b31bd937d34eaaf7a267f12f4e3bb84b0cf21
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-03 20:29:06 +0800

    Fix indents

--- spec/requests/session_spec.rb
+++ spec/requests/session_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 RSpec.describe "Session", :type => :request do
-before(:each) do
-    @user =  FactoryBot.create(:user) 
+  before(:each) do
+    @user = FactoryBot.create(:user) 
     @sign_in_url = '/auth/sign_in' 
     @sign_out_url = '/auth/sign_out'
     @login_params = {
@@ -9,36 +9,35 @@ before(:each) do
         password: @user.password
     }
   end
-describe 'POST /auth/sign_in' do
-context 'when login params is valid' do
+  describe 'POST /auth/sign_in' do
+    context 'when login params is valid' do
       before do
         post @sign_in_url, params: @login_params, as: :json
       end
-it 'returns status 200' do
+      it 'returns status 200' do
         expect(response).to have_http_status(200)
       end
-it 'returns access-token in authentication header' do
+      it 'returns access-token in authentication header' do
         expect(response.headers['access-token']).to be_present
       end
-it 'returns client in authentication header' do
+      it 'returns client in authentication header' do
         expect(response.headers['client']).to be_present
       end
-it 'returns expiry in authentication header' do
+      it 'returns expiry in authentication header' do
         expect(response.headers['expiry']).to be_present
       end
-it 'returns uid in authentication header' do
+      it 'returns uid in authentication header' do
         expect(response.headers['uid']).to be_present
       end
     end
-context 'when login params is invalid' do
+    context 'when login params is invalid' do
       before { post @sign_in_url }
-it 'returns unathorized status 401' do
+      it 'returns unathorized status 401' do  
         expect(response.status).to eq 401
       end
     end
-end
-describe 'DELETE /auth/sign_out' do
-   
+  end
+  describe 'DELETE /auth/sign_out' do
     before do
       #login @user created in the beore block in outer describe block
       post @sign_in_url, params: @login_params, as: :json
@@ -48,7 +47,7 @@ describe 'DELETE /auth/sign_out' do
         'access-token' => response.headers['access-token']
       }
     end
-it 'returns status 200' do
+    it 'returns status 200' do
       delete @sign_out_url, headers: @headers
       expect(response).to have_http_status(200)
     end
