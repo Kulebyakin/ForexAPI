db89710abdbe54818b4e1b6dfe018c2daf6489a0
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-04 00:04:17 +0800

    Validate amount as greater_than_or_equal_to 0

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,5 +4,5 @@ class Account < ApplicationRecord
   has_many :from_order, class_name: 'Order', inverse_of: :from_account
   has_many :to_order, class_name: 'Order', inverse_of: :to_account
 
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
