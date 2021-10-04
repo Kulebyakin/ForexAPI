9fd78d19d06bbdeb02afae5f6019aaa2e73e2c0c
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-10-04 12:05:02 +0800

    Fix association

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,8 @@
 class Account < ApplicationRecord
   belongs_to :user
   belongs_to :currency
-  has_many :from_order, class_name: 'Order', inverse_of: :from_account
-  has_many :to_order, class_name: 'Order', inverse_of: :to_account
+  has_many :from_order, class_name: 'Order', inverse_of: :from_account, foreign_key: "from_account_id"
+  has_many :to_order, class_name: 'Order', inverse_of: :to_account, foreign_key: "to_account_id"
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
